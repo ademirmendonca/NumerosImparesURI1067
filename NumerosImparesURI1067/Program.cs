a731d46564d38ded179adf52fe2468acb2b424ba
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-07 23:54:13 -0300

    Add project files.

---
+++ NumerosImparesURI1067/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumerosImparesURI1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            //Faz a leitura de um numero informado pelo usuario
+            x = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= x; i++) 
+            {
+                
+                //Imprime todos os numeros impares, inclusive o informado se for o caso
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
